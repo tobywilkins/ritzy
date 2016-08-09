cb2b5cd92ff7b7e88768d88593250f99cf393c4e
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-08-09 13:52:56 +0100

    set constraints on the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+
 
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
-
+  
 end
