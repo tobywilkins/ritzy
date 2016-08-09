eaea845424954888949d3dd9490c04bf01bfeb83
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-08-09 14:28:28 +0100

    created first api end point to show users

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-
   devise_for :users
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
-  
 end
