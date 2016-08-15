292272cfa80f6269a07a383988db1f6c76bef175
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-08-15 09:41:19 +0100

    removed api subdomain for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
-#   devise_for :users, path_names: {
-#   sign_up: ''
-# }
   namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
+   path: '/'  do
     scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
       get 'swipes', to: 'swipe#show'
       post 'swipes', to: 'swipe#create'
